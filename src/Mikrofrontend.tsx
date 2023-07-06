6bd0ce72433c0d98b6d0b881f303b210987d84d3
MartheFH <136322563+MartheFH@users.noreply.github.com>
2023-07-06 11:17:06 +0200

    Legge til designsystemet (#5)

--- src/Mikrofrontend.tsx
+++ src/Mikrofrontend.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import '@navikt/ds-css'
+import { Alert } from '@navikt/ds-react'
 
 const Mikrofrontend = () => {
-  return <h1>Hello Deltakerlisten</h1>
+  return (
+    <>
+      <h1>Hello Deltakerlisten</h1>
+      <p>Test designsystem</p>
+      <Alert variant="success">Valp og Komet er venner</Alert>
+    </>
+  )
 }
 
 export default Mikrofrontend
